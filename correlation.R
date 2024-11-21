086e841de5fcba7eff5be715bcffe1c7890cc55e
Gopalakrishnapothumathi <gp24aas@herts.ac.uk>
2024-11-21 07:34:36 +0000

    removed comparison of happiness score by country

--- correlation.R
+++ correlation.R
@@ -1,13 +1,7 @@
 #add the data set
 data<-read.csv("./Assests/2017.csv")
 print(data)
-boxplot(Happiness.Score ~ Country, data = data, 
-        main = "Comparison of Happiness Score by Country",
-        xlab = "Country", 
-        ylab = "Happiness Score",
-        las = 2,
-        varwidth=TRUE,# Rotate x-axis labels for readability
-        bg = "red")
+
 plot(data$Economy..GDP.per.Capita., data$Happiness.Score,
      main = "Correlation between GDP per Capita and Happiness Score",
      xlab = "Economy (GDP per Capita)",
