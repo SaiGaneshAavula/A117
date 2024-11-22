1540d97331feef6114bcda03bbc89bc86b5e43ac
ga23acb <ga23acb@herts.ac.uk>
2024-11-22 08:37:00 +0000

    added histogram

--- visualisation.R
+++ visualisation.R
@@ -1,3 +1,11 @@
 #add the data set
 data<-read.csv("./Assests/2017.csv")
 print(data)
+hist(data$Happiness.Score,
+     main = "Histogram of Happiness Score with Normal Curve Overlay",  
+     xlab = "Happiness Score",
+     ylab = "Density",
+     col = "lightblue",
+     border = "black",
+     freq = FALSE)  # Set freq = FALSE for density plot
+
