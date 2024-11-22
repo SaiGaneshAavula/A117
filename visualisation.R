ba6323f6dfd0e89db4f1f1e6f1357661198b078a
Sravani-Kumari-Palem <sp24aau@herts.ac.uk>
2024-11-22 08:47:26 +0000

    added normal distributed curve

--- visualisation.R
+++ visualisation.R
@@ -9,3 +9,9 @@ hist(data$Happiness.Score,
      border = "black",
      freq = FALSE)  # Set freq = FALSE for density plot
 
+# Add the normal distribution curve
+x_vals <- seq(min(data$Happiness.Score)-1, max(data$Happiness.Score)+1, length = 100)
+y_vals <- dnorm(x_vals, mean = mean(data$Happiness.Score, na.rm = TRUE), 
+                sd = sd(data$Happiness.Score, na.rm = TRUE))
+lines(x_vals, y_vals, col = "red", lwd = 2)
+
